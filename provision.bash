28ad9ae7ba4b3fb7e3b94106fa187a0af2a0f582
Chmouel Boudjnah <chmouel@chmouel.com>
2018-07-09 18:20:28 +0200

    Readd multi container support with MongoDB

--- provision.bash
+++ provision.bash
@@ -9,10 +9,13 @@ REPO="https://github.com/chmouel/nodejs-ex"
 
 reset() {
     for i in ${TARGET_USER} ${TARGET_USER}-run ${TARGET_USER}-stage;do
-        for t in is bc service istag route dc;do
+        for t in configmap is pvc secrets bc service istag route dc;do
             oc delete $t --all -n $i
         done
+        oc delete all --all -n $i
     done
+    # Go figure,
+    oc delete bc --all -n ${TARGET_USER}
 }
 
 reset
@@ -21,7 +24,7 @@ oc process -f osio-pipeline-build.yaml TARGET_USER=$TARGET_USER \
    SOURCE_REPOSITORY_URL=${REPO}|oc create -n ${TARGET_USER} -f-
 
 oc process -f osio-pipeline-run.yaml TARGET_USER=$TARGET_USER \
-   AUTOMATIC_DEPLOY=true|oc create -n ${TARGET_USER}-stage -f-
+   |oc create -n ${TARGET_USER}-stage -f-
 
 oc process -f osio-pipeline-run.yaml TARGET_USER=$TARGET_USER \
-   AUTOMATIC_DEPLOY=false|oc create -n ${TARGET_USER}-run -f-
+   |oc create -n ${TARGET_USER}-run -f-
